99c24e06e99221f166ac85b79740e23999488470
lsw <hkbvc1222@gmail.com>
2024-10-16 23:43:21 +0900

    chore: type 정의

---
+++ src/basic/index.d.ts
@@ -0,0 +1,14 @@
+type ProductType = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  discount: number;
+};
+
+type CartProductType = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
