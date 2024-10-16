47d661a124e29619b2d85929895389f72d8f408d
lsw <hkbvc1222@gmail.com>
2024-10-16 23:46:09 +0900

    feat: style

---
+++ src/basic/style/index.ts
@@ -0,0 +1,22 @@
+export const styleClass = {
+  container: 'bg-gray-100 p-8',
+
+  'card-wrap':
+    'max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8',
+
+  'card-title': 'text-2xl font-bold mb-4',
+
+  'total-price': 'text-xl font-bold my-4',
+
+  'select-item': 'border rounded p-2 mr-2',
+
+  'add-btn': 'bg-blue-500 text-white px-4 py-2 rounded',
+
+  'stock-status': 'text-sm text-gray-500 mt-2',
+
+  'cart-item-wrap': 'flex justify-between items-center mb-2',
+
+  'cart-btn': 'bg-blue-500 text-white px-2 py-1 rounded mr-1',
+
+  'cart-remove-btn': 'bg-red-500 text-white px-2 py-1 rounded mr-1',
+};
